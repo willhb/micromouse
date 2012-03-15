54db4fcc7362461f6af23b95c16ed9f096afb4eb
William Breidenthal <willhb@gmail.com>
2012-03-14 21:28:32 -0700

    Started work on encoder software.

--- main.c
+++ main.c
@@ -7,10 +7,10 @@
 #define SENSOR_R        ((AD1DR0 >> 6) & 0x3ff)         // index 0
 
 void uart0_init(void);
+void uart1_init(void);
 
-void uart0_init(void){
-	
-	PINSEL0=0x5; //Enable UART0 on P0.0(TX) and P0.1(RX)
+void uart0_init(void){	
+	PINSEL0 |= (1<<0) | (1<<2); //Enable UART0 on P0.0(TX) and P0.1(RX)
 	U0FCR = 0x07;
 	U0LCR = 0x83;
 	U0DLL = 32; 
@@ -18,38 +18,103 @@ void uart0_init(void){
 	U0LCR = 0x03; 
 }
 
-
-void sright(int speed){
-	U0THR = 0xC2;
-    U0THR = speed;
-    U0THR = 0xC5;
-    U0THR = speed;
-	delay_ms(590);
-	stop();
+void uart1_init(void){	
+	PINSEL0 |= (1<<16) | (1<<18); //Enable UART1 on P0.8(TX) and P0.9(RX)
+	U1FCR = 0x07;
+	U1LCR = 0x83;
+	U1DLL = 56; 
+	U1DLM = 1;
+	U1LCR = 0x03; 
 }
 
+int lcount = 0;
+int rcount = 0;
+
+void left_enc_ISR(){
+	lcount++;
+	printf("Left tick # %d\n\r", lcount);
+	if(lcount == 12){
+		U0THR = 0xC1;
+		U0THR = 0;
+	}
+	EXTINT = (1<<2); //Clear interrupt flag on EINT2
+}
+
+void right_enc_ISR(){
+	rcount++;
+	printf("Right tick  # %d\n\r", rcount);
+	if(rcount == 12){
+		U0THR = 0xC6;
+		U0THR = 0;
+	}
+	EXTINT = (1<<0); //Clear interrupt flag on EINT0
+}
 
 int	main (void)
 {	
-	// initialize the LED pin as an output
-	uart0_init();
-	FIO1DIR |= 1 << 26 | 1 << 20;
-	FIO1DIR |= (1<<30) | (1<<29) | (1<<28);
-	FIO1CLR = (1<<30) | (1<<29) | (1<<28);
-	FIO1SET = (1<<30) | (1<<29) | (1<<28);
+	FIO1DIR |=  1 << 20;	//Use OpenLPC Green LED
+	FIO1SET  =	1 << 20;	//Turn off LED during initialization.
+	
+	uart0_init();	//Initialize UART0 for 3pi motor comm.
+	uart1_init();	//Initialize UART1 for Xbee debugging.
+	
+	FIO1DIR |= (1<<30) | (1<<29) | (1<<28);	//Pins 1.28,1.29, and 1.30 control IR L,R, and F emitters. 
+	FIO1CLR = (1<<30) | (1<<29) | (1<<28);	//Turn off all IR LEDs
+	
+	PINSEL0 |= (3 << 12) | (3 << 20) | (3 << 24) | (3 << 26); //Setup P1.0/2/3/4 as ADC inputs
+	
+	PINSEL0 |= 1 << 31; //Setup P1.15 as EINT2
+	PINSEL1 |= 1 << 0;	//Setup P1.16 as EINT0
+	
+	EXTMODE = (1 << 0) | (1 << 2); //EINT0 and EINT2 are edge sensitive.
+	EXTPOLAR = (1 << 0) | (1 << 2); //Rising edge sensitive. 
+	
+	printf("Starting up...\n\r");	//Let anyone debugging through USB know we are around.
+	FIO1CLR  =	1 << 20;			//Turn on status LED. 
+	
+	VIC_registerIRQ(right_enc_ISR, EINT0_IRQn, 2);	//EINT0 is the right wheel encoder.
+	VIC_registerIRQ(left_enc_ISR, EINT2_IRQn, 1);	//EINT2 is the left wheel encoder.
+	
+	EXTINT = (1<<0)|(1<<2);	//Clear any interrupt bits before starting.
+	
+	VIC_enableIRQ(EINT0_IRQn);	//Enable interrupts on EINT0
+	VIC_enableIRQ(EINT2_IRQn);	//Enable interrupts on EINT2
+	
+	//AD1CR = (1 << 0) | (1 << 1) | (1 << 2) | (13 << 8) | (1 << 16) | (1 << 21);
+	
+	//FIO1SET = (1<<30) | (1<<29) | (1<<28);
 	// RIGHT 	MIDDLE 		LEFT
-	PINSEL0 |= (3 << 12) | (3 << 16) | (3 << 20);
-	AD1CR = (1 << 0) | (1 << 1) | (1 << 2) | (13 << 8) | (1 << 16) | (1 << 21);
 	
-	int o = 0;	
-	int mhold = 0;
-	FIO1SET = (1<<30) | (1<<29) | (1<<28);
+	U0THR = 0xC1;
+	U0THR = 20;
+	U0THR = 0xC6;
+	U0THR = 20;
 	
-	int front,left,right;
+	int cmd = 0;
 	
-	int adiff,diff;
-	
-	int interval = millis();
+	while(1){
+		
+		cmd = getc(stdin);
+		if(cmd == 'r'){
+			lcount = 0;
+			rcount = 0;
+			U0THR = 0xC1;
+			U0THR = 20;
+			U0THR = 0xC6;
+			U0THR = 20;
+		}
+		if(cmd == 'l'){
+			lcount = 0;
+			rcount = 0;
+			U0THR = 0xC2;
+			U0THR = 20;
+			U0THR = 0xC5;
+			U0THR = 20;
+		}
+		
+	}
+
+
 	
 
 }
